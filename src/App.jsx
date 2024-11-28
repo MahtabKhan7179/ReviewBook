5586fb0bab73cfc2047cdee7ef7184bc0659f61b
Mahtab Khan <khan48716@gmail.com>
2024-11-28 08:46:15 +0530

    structure updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
 import Reviews from "./data"
 import Person from "./components/person";
-import { FaGreaterThan, FaLessThan } from "react-icons/fa";
 
 
 const App = () => {
-  const [reviews, setReviews] = useState(Reviews);
-  const [index, setIndex] = useState(0);
 
-  return <div className="review">
-    <Person review={reviews[index]} />
-    <div className="btn-container">
-      <FaLessThan className="prev-btn" onClick={() => { setIndex(Math.abs(reviews.length + index - 1) % reviews.length) }} />
-      <FaGreaterThan className="next-btn" onClick={() => { setIndex((index + 1) % reviews.length) }} />
-    </div>
-    <button className="btn btn-block" onClick={() => { setIndex(Math.floor(Math.random() * reviews.length) + 0) }}>Surprise Me</button>
-  </div >;
+  return <main>
+    <Person reviews={Reviews} />
+  </main>;
 };
 export default App;
--- src/components/person.jsx
+++ src/components/person.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { FaQuoteRight } from 'react-icons/fa';
+import { FaGreaterThan, FaLessThan } from "react-icons/fa";
 
-export default function Person({ review }) {
-    const { id, name, job, image, text } = review;
-    return (<article>
+export default function Person({ reviews }) {
+    const [index, setIndex] = useState(0);
+    const { id, name, job, image, text } = reviews[index];
+
+    return (<article className="review">
         <div className="img-container">
             <img className="person-img" src={image} alt="text" />
             <div className="quote-icon"><FaQuoteRight /></div>
         </div>
-        <div>
-            <h4 className="author">{name}</h4>
-            <p className="job">{job}</p>
-            <p className="info">{text}</p>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+            <FaLessThan className="prev-btn" onClick={() => { setIndex(Math.abs(reviews.length + index - 1) % reviews.length) }} />
+            <FaGreaterThan className="next-btn" onClick={() => { setIndex((index + 1) % reviews.length) }} />
         </div>
+        <button className="btn btn-block" onClick={() => { setIndex(Math.floor(Math.random() * reviews.length) + 0) }}>Surprise Me</button>
+
     </article>);
 }
